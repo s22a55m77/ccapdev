b66946c40a7c44377814d8a5e250584428f95508
s22a55m77 <s22a55m77@gmail.com>
2023-09-15 19:02:19 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 75,
+  proseWrap: 'never',
+  endOfLine: 'crlf',
+};
