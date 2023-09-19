532337d5ad948be83b5d39e7f7b976d00eed6678
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-09-19 11:44:21 +0800

    add filter

--- apps/front/src/components/Main/index.tsx
+++ apps/front/src/components/Main/index.tsx
@@ -1,12 +1,34 @@
 import './index.css'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CallMadeIcon from '@mui/icons-material/CallMade';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import {Avatar, Card, CardContent, CardHeader} from "@mui/material";
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import {Button, Menu, MenuItem, Radio} from "@mui/material";
+import {useState} from "react";
+import RestroomCard from "./components/RestroomCard.tsx";
 
 export default function Main() {
-  // const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorMenuEl, setAnchorMenuEl] = useState<null | HTMLElement>(null);
+  const [selectedBuilding, setSelectedBuilding] = useState<string>('');
+  const [selectedFloor, setSelectedFloor] = useState<string>('');
+  const open = Boolean(anchorEl);
+  const openMenu = Boolean(anchorMenuEl);
+
+  const handleFilterClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleFilterClose = () => {
+    setAnchorEl(null);
+    setAnchorMenuEl(null);
+    setSelectedBuilding('')
+  };
+
+  const handleMenuClick = () => {
+    setAnchorMenuEl(anchorEl)
+    setAnchorEl(null)
+  }
 
   return (
     <div className={'main-container'}>
@@ -16,40 +38,140 @@ export default function Main() {
             <AccessTimeIcon fontSize={'inherit'} style={{marginRight: 5}}/> New
           </div>
         </button>
-        <button className={'tab-button'}>
+        <button className={'tab-button'} >
           <div style={{display: 'flex'}}>
             <CallMadeIcon fontSize={'inherit'} style={{marginRight: 5}}/> Top
           </div>
         </button>
+        <button className={'tab-button'} id="basic-button" onClick={handleFilterClick}>
+          <div style={{display: 'flex'}}>
+            <FilterAltIcon fontSize={'inherit'} style={{marginRight: 5}}/> Filter
+          </div>
+        </button>
       </div>
-      <div id={'cards'} className={'card-container'}>
-        <Card sx={{maxWidth: '100%'}} className={'card'}>
-          {/*fix me */}
-          <CardHeader
-            avatar={
-              <Avatar />
-            }
-            title="Placeholder"
-            subheader="September 18, 2023"
+
+
+      <RestroomCard />
+
+      {/*  MENU Building*/}
+      <Menu
+        id="filter-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleFilterClose}
+        PaperProps={{
+          style: {
+            padding: '0px',
+            width: '200px',
+          }
+        }}
+      >
+        <MenuItem
+          style={{
+            padding: '0px'
+          }}
+        >
+          <Radio
+            checked={selectedBuilding === 'Gokongwei'}
+            onChange={(e) => setSelectedBuilding(e.target.value)}
+            name='building'
+            value='Gokongwei'
+            size='small'
           />
-          <CardContent>
-            <div id='title'><b>Title</b></div>
-            <div className={'card-content'}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat aliquet maecenas ut sit nulla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consequat aliquet maecenas ut sit nulla </div>
-            {/*fix it*/}
-            <img style={{marginTop: 10}} src={'src/assets/toilet.png'} alt={'img'} width={'30%'}/>
-            <footer className={'card-footer'}>
-              <div id='tags' className={'tags'}>
-                <div className={'tag'}>Tag</div>
-                <div className={'tag'}>Tag</div>
-              </div>
-              <div className={'activity'}>
-                <div className={'activity-content'}><ChatBubbleOutlineIcon fontSize={'inherit'}/> 1 </div>
-                <div className={'activity-content'}><ArrowUpwardIcon fontSize={'inherit'}/> 1 </div>
-              </div>
-            </footer>
-          </CardContent>
-        </Card>
-      </div>
+          <Button onClick={handleMenuClick} endIcon={<ArrowForwardIosIcon/>} color={'green'} className={'menu-button'} style={{justifyContent: 'end'}}>
+            <span className={'text-wrap'}>Gokongwei Hall</span>
+          </Button>
+        </MenuItem>
+        <MenuItem
+          style={{
+            padding: '0px'
+          }}
+        >
+          <Radio
+            checked={selectedBuilding === 'Razon'}
+            onChange={(e) => setSelectedBuilding(e.target.value)}
+            name='building'
+            value='Razon'
+            size='small'
+          />
+          <Button onClick={handleMenuClick} endIcon={<ArrowForwardIosIcon/>} color={'green'} className={'menu-button'} style={{justifyContent: 'end'}}>
+            <span className={'text-wrap'}>Razon</span>
+          </Button>
+        </MenuItem>
+        <MenuItem
+          style={{
+            padding: '0px'
+          }}
+        >
+          <Radio
+            checked={selectedBuilding === 'Henry Sy'}
+            onChange={(e) => setSelectedBuilding(e.target.value)}
+            name='building'
+            value='Henry Sy'
+            size='small'
+          />
+          <Button onClick={handleMenuClick} endIcon={<ArrowForwardIosIcon/>} color={'green'} className={'menu-button'} style={{justifyContent: 'end'}}>
+            <span className={'text-wrap'}>Henry Sy</span>
+          </Button>
+        </MenuItem>
+      </Menu>
+
+      {/*  MENU Floor*/}
+      <Menu
+        id="filter-menu"
+        anchorEl={anchorMenuEl}
+        open={openMenu}
+        onClose={handleFilterClose}
+      >
+        <MenuItem
+          style={{
+            padding: '0px',
+            paddingLeft: '3px',
+            paddingRight: '10px'
+          }}
+        >
+          <Radio
+            checked={selectedFloor === '1'}
+            onChange={(e) => setSelectedFloor(e.target.value)}
+            name='floor'
+            value='1'
+            size='small'
+          />
+          <span className={'green'}>1</span>
+        </MenuItem>
+        <MenuItem
+          style={{
+            padding: '0px',
+            paddingLeft: '3px',
+            paddingRight: '10px'
+          }}
+        >
+          <Radio
+            checked={selectedFloor === '2'}
+            onChange={(e) => setSelectedFloor(e.target.value)}
+            name='floor'
+            value='2'
+            size='small'
+          />
+          <span className={'green'}>2</span>
+        </MenuItem>
+        <MenuItem
+          style={{
+            padding: '0px',
+            paddingLeft: '3px',
+            paddingRight: '10px'
+          }}
+        >
+          <Radio
+            checked={selectedFloor === '3'}
+            onChange={(e) => setSelectedFloor(e.target.value)}
+            name='floor'
+            value='3'
+            size='small'
+          />
+          <span className={'green'}>3</span>
+        </MenuItem>
+      </Menu>
     </div>
   )
 }
