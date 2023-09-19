3b34293ef46b4920b916b02e51fe2d4dca528dc7
s22a55m77 <s22a55m77@gmail.com>
2023-09-19 11:18:58 +0800

    add api function for update user profile pic, restroom list, restroom detail, rate restroom, create restroom

--- apps/front/src/services/mock.ts
+++ apps/front/src/services/mock.ts
@@ -58,4 +58,182 @@ export default function configureMock(mock: MockAdapter) {
       description: 'this is the new description',
     }),
   );
+
+  // GET /restroom
+  // query building
+  // query floor
+  // query sort
+  mock.onGet('/restroom').reply((config) => {
+    let restrooms: API.RestroomListData = [
+      {
+        id: '1',
+        title: 'Restroom 1',
+        tags: ['tag1', 'tag2'],
+        building: 'building 1',
+        floor: 1,
+        rating: 4,
+        restroomImageIds: ['1'],
+        gender: 'MALE',
+      },
+      {
+        id: '2',
+        title: 'Restroom 2',
+        tags: ['tag1', 'tag2'],
+        building: 'building 2',
+        floor: 3,
+        rating: 2,
+        restroomImageIds: ['2'],
+        gender: 'FEMALE',
+      },
+    ];
+
+    if (config.params.building) {
+      restrooms = restrooms.filter(
+        (restroom) => restroom.building === config.params.building,
+      );
+    }
+
+    if (config.params.floor) {
+      restrooms = restrooms.filter(
+        (restroom) => restroom.floor === config.params.floor,
+      );
+    }
+
+    if (config.params.rating) {
+      if (config.params.rating === 'ASC') {
+        restrooms = restrooms.slice().sort((a, b) => a.rating - b.rating);
+      }
+
+      if (config.params.rating === 'DESC') {
+        restrooms = restrooms.slice().sort((a, b) => b.rating - a.rating);
+      }
+    }
+
+    const response: API.RestroomListResponse =
+      createResponse<API.RestroomListData>(restrooms);
+
+    return [200, response];
+  });
+
+  // GET /restroom/:id
+  mock.onGet(/^\/restroom\/(\d+)$/).reply((config) => {
+    let restrooms: API.RestroomData[] = [
+      {
+        id: '1',
+        title: 'Restroom 1',
+        tags: ['tag1', 'tag2'],
+        building: 'building 1',
+        floor: 1,
+        rating: 4,
+        restroomImageIds: ['1'],
+        location: 'location description',
+        locationImageIds: ['3'],
+        commentsIds: ['1'],
+        gender: 'MALE',
+      },
+      {
+        id: '2',
+        title: 'Restroom 2',
+        tags: ['tag1', 'tag2'],
+        building: 'building 2',
+        floor: 3,
+        rating: 2,
+        restroomImageIds: ['2'],
+        location: 'location description',
+        locationImageIds: ['4'],
+        commentsIds: ['2'],
+        gender: 'FEMALE',
+      },
+    ];
+
+    if (config.url) {
+      const result = config.url.match(/^\/restroom\/(\d+)$/);
+      if (result) {
+        const id = result[0];
+
+        const restroom = restrooms.filter(
+          (restroom) => restroom.id === id,
+        )[0];
+
+        const response: API.RestroomDetailResponse =
+          createResponse(restroom);
+
+        return [200, response];
+      }
+    }
+
+    return [400];
+  });
+
+  // POST /restroom
+  mock.onPost('/restroom').reply((config) => {
+    const restroom: API.RestroomData = {
+      id: '100',
+      title: `${config.data.building}-${config.data.floor} floor-${config.data.gender}`,
+      building: config.data.building,
+      floor: config.data.floor,
+      location: config.data.location,
+      rating: 2,
+      tags: [],
+      locationImageIds: config.data.locationImageIds,
+      restroomImageIds: config.data.restroomImageIds,
+      commentsIds: [],
+      gender: config.data.gender,
+    };
+
+    const response: API.CreateRestroomResponse = createResponse(restroom);
+
+    return [200, response];
+  });
+
+  // POST /restroom/rate
+  mock.onPost('/restroom/rate').reply((config) => {
+    let restrooms: API.RestroomData[] = [
+      {
+        id: '1',
+        title: 'Restroom 1',
+        tags: ['tag1', 'tag2'],
+        building: 'building 1',
+        floor: 1,
+        rating: 4,
+        restroomImageIds: ['1'],
+        location: 'location description',
+        locationImageIds: ['3'],
+        commentsIds: ['1'],
+        gender: 'MALE',
+      },
+      {
+        id: '2',
+        title: 'Restroom 2',
+        tags: ['tag1', 'tag2'],
+        building: 'building 2',
+        floor: 3,
+        rating: 2,
+        restroomImageIds: ['2'],
+        location: 'location description',
+        locationImageIds: ['4'],
+        commentsIds: ['2'],
+        gender: 'FEMALE',
+      },
+    ];
+
+    const restroom = restrooms.filter(
+      (restroom) => restroom.id === config.data.restroomId,
+    )[0];
+
+    restroom.rating = config.data.rating;
+
+    const response: API.RateRestroomResponse =
+      createResponse<API.RestroomData>(restroom);
+
+    return [200, response];
+  });
+
+  // PATCH /user/profile/pic
+  mock.onPatch('/user/profile/pic').reply(
+    200,
+    createResponse<API.UpdateProfilePicData>({
+      id: '1',
+    }),
+  );
 }
