7b2e4750983bf372edb6ee953da09f03bc9323ea
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-09-23 19:27:08 +0800

    add animation

--- apps/front/src/pages/Login/index.tsx
+++ apps/front/src/pages/Login/index.tsx
@@ -1,49 +1,62 @@
 import './index.css';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
   return (
     <section className="login-section">
-      <div className="form-box">
-        <div className="form-value">
-          <form action="">
-            <h2 className="login-h2">Login</h2>
-            <div className="inputbox">
-              <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-              <input
-                type="email"
-                id="email"
-                placeholder=" "
-                required
-                aria-label="Email"
-              />
-              <label htmlFor="email">Email</label>
-            </div>
-            <div className="inputbox">
-              <ion-icon name="lock-closed-outline"></ion-icon>
-              <input
-                type="password"
-                id="password"
-                placeholder=" "
-                required
-              />
-              <label htmlFor="password">Password</label>
-            </div>
-            <div className="forget">
-              <label>
-                <input type="checkbox" />
-                Remember Me
-                <a href="#">Forget Password</a>
-              </label>
-            </div>
-            <button className="login-button">Log in</button>
-            <div className="register">
-              <p>
-                Don't have an account? <a href="register">Register Now!</a>
-              </p>
-            </div>
-          </form>
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, x: 50, backdropFilter: 'blur(20px)' }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 50 }}
+      >
+        <div className="form-box">
+          <div className="form-value">
+            <form action="">
+              <h2 className="login-h2">Login</h2>
+              <div className="inputbox">
+                <ion-icon
+                  name="mail-outline"
+                  aria-hidden="true"
+                ></ion-icon>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder=" "
+                  required
+                  aria-label="Email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="inputbox">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder=" "
+                  required
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <div className="forget">
+                <label>
+                  <input type="checkbox" />
+                  Remember Me
+                  <a href="#">Forget Password</a>
+                </label>
+              </div>
+              <button className="login-button">Log in</button>
+              <div className="register">
+                <p>
+                  Don't have an account?{' '}
+                  <Link to="/register">Register Now!</Link>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
--- apps/front/src/pages/Register/index.tsx
+++ apps/front/src/pages/Register/index.tsx
@@ -1,52 +1,71 @@
 import './index.css';
+import { motion } from 'framer-motion';
 
 export default function RegisterPage() {
   return (
     <section className="register-section">
-      <div className="form-box">
-        <div className="form-value">
-          <form action="">
-            <h2 className="register-h2">Register</h2>
-            <div className="inputbox">
-              <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-              <input type="text" id="username" placeholder=" " required />
-              <label htmlFor="username">Username</label>
-            </div>
-            <div className="inputbox">
-              <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-              <input
-                type="email"
-                id="email"
-                placeholder=" "
-                required
-                aria-label="Email"
-              />
-              <label htmlFor="email">Email</label>
-            </div>
-            <div className="inputbox">
-              <ion-icon name="lock-closed-outline"></ion-icon>
-              <input
-                type="password"
-                id="password1"
-                placeholder=" "
-                required
-              />
-              <label htmlFor="password1">Password</label>
-            </div>
-            <div className="inputbox">
-              <ion-icon name="lock-closed-outline"></ion-icon>
-              <input
-                type="password"
-                id="password2"
-                placeholder=" "
-                required
-              />
-              <label htmlFor="password2">Repeat Password</label>
-            </div>
-            <button className="register-button">Register</button>
-          </form>
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0, x: 50, backdropFilter: 'blur(20px)' }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 50 }}
+      >
+        <div className="form-box">
+          <div className="form-value">
+            <form action="">
+              <h2 className="register-h2">Register</h2>
+              <div className="inputbox">
+                <ion-icon
+                  name="mail-outline"
+                  aria-hidden="true"
+                ></ion-icon>
+                <input
+                  type="text"
+                  id="username"
+                  placeholder=" "
+                  required
+                />
+                <label htmlFor="username">Username</label>
+              </div>
+              <div className="inputbox">
+                <ion-icon
+                  name="mail-outline"
+                  aria-hidden="true"
+                ></ion-icon>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder=" "
+                  required
+                  aria-label="Email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="inputbox">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input
+                  type="password"
+                  id="password1"
+                  placeholder=" "
+                  required
+                />
+                <label htmlFor="password1">Password</label>
+              </div>
+              <div className="inputbox">
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input
+                  type="password"
+                  id="password2"
+                  placeholder=" "
+                  required
+                />
+                <label htmlFor="password2">Repeat Password</label>
+              </div>
+              <button className="register-button">Register</button>
+            </form>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
