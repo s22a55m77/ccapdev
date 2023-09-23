a9e7c1ab68499c81f40ec11e991d99b8cb9ffcd8
s22a55m77 <s22a55m77@gmail.com>
2023-09-23 15:43:43 +0800

    rename components to pages

--- apps/front/src/router.tsx
+++ apps/front/src/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
-import Main from './components/Main';
-import Restroom from './components/Restroom';
+import Main from './pages/Main';
+import Restroom from './pages/Restroom';
 const router = createBrowserRouter([
   {
     path: '/',
