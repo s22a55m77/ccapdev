5a643e74f9519daa7d9b7ec9224dc0c3f6a8740a
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-09-25 11:02:50 +0800

    add login mode navbar

--- apps/front/src/pages/Navbar/index.tsx
+++ apps/front/src/pages/Navbar/index.tsx
@@ -1,22 +1,84 @@
 import logo from '../../assets/logo.png';
-import './index.css'
-import {Button} from "@mui/material";
+import './index.css';
+import { Avatar, Button, Menu, MenuItem } from '@mui/material';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonIcon from '@mui/icons-material/Person';
+import { useState } from 'react';
 export default function Navbar() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [avatarAnchor, setAvatarAnchor] = useState<null | HTMLElement>();
+  const open = Boolean(avatarAnchor);
+
+  const handleAvatarClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    setAvatarAnchor(event.currentTarget);
+  };
+
+  const handleAvataClose = () => {
+    setAvatarAnchor(null);
+  };
   return (
     <div className={'nav-container'}>
       <div className={'flex'}>
         <div>
-          <img src={logo} alt='logo' className={'image'} width={40} height={40}/>
+          <img
+            src={logo}
+            alt="logo"
+            className={'image'}
+            width={40}
+            height={40}
+          />
         </div>
         <div className={'v-center m-10'}>
           DLSU toiletTo<b>PICK</b>
         </div>
       </div>
       <div>
-        <Button variant='contained' color={'green'} startIcon={<PersonAddAltIcon/>}>Register</Button>
-        <Button style={{marginLeft: '10px'}} variant='outlined'>Login</Button>
+        {!isLogin ? (
+          <div>
+            <Button
+              variant="contained"
+              color={'green'}
+              startIcon={<PersonAddAltIcon />}
+            >
+              Register
+            </Button>
+            <Button style={{ marginLeft: '10px' }} variant="outlined">
+              Login
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button id="basic-button" onClick={handleAvatarClick}>
+              <Avatar></Avatar>
+            </Button>
+          </div>
+        )}
       </div>
+      <Menu
+        anchorEl={avatarAnchor}
+        open={open}
+        onClose={handleAvataClose}
+        transformOrigin={{
+          vertical: 10,
+          horizontal: 100,
+        }}
+      >
+        <MenuItem>
+          <span className={'user-profile'}>
+            <PersonIcon />
+            User Profile
+          </span>
+        </MenuItem>
+        <MenuItem>
+          <span className={'logout'}>
+            <LogoutIcon />
+            Logout
+          </span>
+        </MenuItem>
+      </Menu>
     </div>
-  )
+  );
 }
