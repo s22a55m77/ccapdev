4408d2967d1ff9b85ec4327302c92fae0376f465
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2023-09-29 12:39:08 +0800

    change layout style

--- apps/front/src/pages/CreateRestroom/index.tsx
+++ apps/front/src/pages/CreateRestroom/index.tsx
@@ -1,5 +1,15 @@
 import './index.css';
 import { motion } from 'framer-motion';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
+import WcOutlinedIcon from '@mui/icons-material/WcOutlined';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 
 export default function Index() {
   return (
@@ -11,31 +21,63 @@ export default function Index() {
         exit={{ opacity: 0, x: 50 }}
         className={'form-box'}
       >
-        <div className="form-value">
+        <div>
           <form action="">
             <h2 className="restroom-form-h2">Restroom forms</h2>
-            <div className="inputbox">
-              <ion-icon name="home-outline"></ion-icon>
-              <input type="text" id="building" placeholder=" " required />
-              <label htmlFor="building">Building</label>
-            </div>
-            <div className="inputbox">
-              <ion-icon name="home-outline"></ion-icon>
-              <input type="text" id="address" placeholder=" " required />
-              <label htmlFor="address">Location</label>
-            </div>
+            <FormControl fullWidth required>
+              <div className={'flex'} style={{ alignItems: 'center' }}>
+                <InputLabel id="building-label">Building</InputLabel>
+                <Select
+                  id="building"
+                  labelId="building-label"
+                  label="Building *"
+                  value="building 1"
+                  sx={{ width: '90%' }}
+                >
+                  <MenuItem value="building 1">building 1</MenuItem>
+                </Select>
+                <LocationOnOutlinedIcon />
+              </div>
+            </FormControl>
             <div className="smallbox">
-              <div className="input-group">
-                <ion-icon name="home-outline"></ion-icon>
-                <input type="number" id="floor" placeholder=" " required />
-                <label htmlFor="floor">Floor</label>
-              </div>
-
-              <div className="input-group">
-                <ion-icon name="people-outline"></ion-icon>
-                <input type="text" id="address" placeholder=" " required autoComplete="off" />
-                <label htmlFor="address">Gender</label>
-              </div>
+              <FormControl fullWidth required>
+                <div className={'flex'} style={{ alignItems: 'center' }}>
+                  <InputLabel id="building-label">Floor</InputLabel>
+                  <Select
+                    id="building"
+                    labelId="building-label"
+                    label="Floor *"
+                    value="floor 1"
+                    sx={{ width: '70%' }}
+                  >
+                    <MenuItem value="floor 1">floor 1</MenuItem>
+                  </Select>
+                  <LayersOutlinedIcon />
+                </div>
+              </FormControl>
+              <FormControl fullWidth required>
+                <div className={'flex'} style={{ alignItems: 'center' }}>
+                  <InputLabel id="building-label">Gender</InputLabel>
+                  <Select
+                    id="building"
+                    labelId="building-label"
+                    label="Gender *"
+                    value="Male"
+                    sx={{ width: '80%' }}
+                  >
+                    <MenuItem value="Male">Male</MenuItem>
+                    <MenuItem value="Male">Female</MenuItem>
+                  </Select>
+                  <WcOutlinedIcon />
+                </div>
+              </FormControl>
+            </div>
+            <div>
+              <TextField
+                label={'Location Description'}
+                multiline
+                sx={{ width: '90%' }}
+              />
             </div>
             <p className={'restroom-form-p'}>Availability:</p>
             <div className="availbox">
