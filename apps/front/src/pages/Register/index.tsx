49038c036dc5c4bd44eb9d637f526e3f2235f10d
KennyTeear <kendrick_chan@dlsu.edu.ph>
2023-10-02 01:00:01 +0800

    bonk

--- apps/front/src/pages/Register/index.tsx
+++ apps/front/src/pages/Register/index.tsx
@@ -1,13 +1,21 @@
 import './index.css';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { me, register as registerUser } from '../../services/api.ts';  
+import { useUser } from '../../pages/Login/user.store.ts';
 
 export default function RegisterPage() {
-  /* TODO 通过react-hook-form拿到用户输入的账号密码，调用注册接口
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [errMsg, setErrMsg] = useState('');
+  const { setUser } = useUser();
+  const navigate = useNavigate();
+    /* TODO 通过react-hook-form拿到用户输入的账号密码，调用注册接口
   成功: 调用me接口拿到详细的用户资讯，调用user.store.ts里的setUser把接口返回的数据存起来，跳转到 /
   失败: 给出相应的错误讯息 可能的错误: 账号已经存在，密码不符合格式，Internal Server Error
   */
-
+ 
   /* TODO Form Validation
   username is not empty
   password is not empty
@@ -15,6 +23,21 @@ export default function RegisterPage() {
 
    在没有通过的情况下 Button是disable状态
   * */
+  const onSubmit = async (data: API.RegisterParams) => {
+    if (data.password1 !== data.password2) {
+      setErrMsg("Passwords do not match");
+      return;
+    }
+
+    try {
+      await registerUser(data);
+      const userData = await me();
+      setUser(userData);
+      navigate("/");
+    } catch (error) {
+      setErrMsg('Error registering, please try again.'); 
+    }
+  };
 
   return (
     <section className="register-section">
@@ -26,31 +49,29 @@ export default function RegisterPage() {
       >
         <div className="register-form-box">
           <div className="form-value">
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <h2 className="register-h2">Register</h2>
+              <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+                {errMsg}
+              </p>
               <div className="inputbox">
-                <ion-icon
-                  name="mail-outline"
-                  aria-hidden="true"
-                ></ion-icon>
+                <ion-icon name="person-outline" aria-hidden="true"></ion-icon>
                 <input
+                  {...register('username', { required: true })}
                   type="text"
                   id="username"
                   placeholder=" "
-                  required
+                  aria-label="Username"
                 />
                 <label htmlFor="username">Username</label>
               </div>
               <div className="inputbox">
-                <ion-icon
-                  name="mail-outline"
-                  aria-hidden="true"
-                ></ion-icon>
+                <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
                 <input
+                  {...register('email', { required: true, pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ })}
                   type="email"
                   id="email"
                   placeholder=" "
-                  required
                   aria-label="Email"
                 />
                 <label htmlFor="email">Email</label>
@@ -58,29 +79,31 @@ export default function RegisterPage() {
               <div className="inputbox">
                 <ion-icon name="lock-closed-outline"></ion-icon>
                 <input
+                  {...register('password1', { required: true, minLength: 6 })}
                   type="password"
                   id="password1"
                   placeholder=" "
-                  required
                 />
                 <label htmlFor="password1">Password</label>
               </div>
               <div className="inputbox">
                 <ion-icon name="lock-closed-outline"></ion-icon>
                 <input
+                  {...register('password2', { required: true, minLength: 6 })}
                   type="password"
                   id="password2"
                   placeholder=" "
-                  required
                 />
                 <label htmlFor="password2">Repeat Password</label>
               </div>
-              <button className="register-button">Register</button>
+              <button className="register-button" disabled={Object.keys(errors).length > 0}>
+                Register
+              </button>
               <div className="login">
-                  <p>
-                    <Link to="/">Go Back &#8594;</Link>
-                  </p>
-                </div>
+                <p>
+                  <Link to="/">Go Back &#8594;</Link>
+                </p>
+              </div>
             </form>
           </div>
         </div>
