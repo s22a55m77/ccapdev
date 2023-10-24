002cf7dea335452afac0cc91b62f8ce168762c12
s22a55m77 <s22a55m77@gmail.com>
2023-10-24 14:19:41 +0800

    add entities

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,6 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BuildingEntity } from './model/building.entity';
+import { CityEntity } from './model/city.entity';
+import { CommentEntity } from './model/comment.entity';
+import { FloorEntity } from './model/floor.entity';
+import { ImageEntity } from './model/image.entity';
+import { ProvinceEntity } from './model/province.entity';
+import { RegionEntity } from './model/region.entity';
+import { ReportEntity } from './model/report.entity';
+import { RestroomEntity } from './model/restroom.entity';
+import { RestroomTagEntity } from './model/restroom-tag.entity';
+import { TagEntity } from './model/tag.entity';
+import { UserEntity } from './model/user.entity';
+import { VoteEntity } from './model/vote.entity';
 
 @Module({
   imports: [
@@ -20,6 +33,21 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         logging: configService.get<boolean>('DB_LOG'),
         ssl: configService.get<boolean>('DB_SSL'),
         migrationsRun: false,
+        entities: [
+          BuildingEntity,
+          CityEntity,
+          CommentEntity,
+          FloorEntity,
+          ImageEntity,
+          ProvinceEntity,
+          RegionEntity,
+          ReportEntity,
+          RestroomEntity,
+          RestroomTagEntity,
+          TagEntity,
+          UserEntity,
+          VoteEntity,
+        ],
         synchronize: configService.get<boolean>('DB_SYNC'),
       }),
       inject: [ConfigService],
