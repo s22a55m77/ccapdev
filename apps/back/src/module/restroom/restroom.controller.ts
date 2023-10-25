7a3cb8844425544b0cc711a1b574d94b2df2e385
hyisangie <angeline_shi@dlsu.edu.ph>
2023-10-25 13:24:04 +0800

    Update restroom.controller.ts

--- apps/back/src/module/restroom/restroom.controller.ts
+++ apps/back/src/module/restroom/restroom.controller.ts
@@ -1,10 +1,41 @@
+<<<<<<< Updated upstream
 import { Body, Controller, Param, Patch } from '@nestjs/common';
+=======
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+>>>>>>> Stashed changes
 import { RestroomService } from './restroom.service';
 import { UpdateRestroomReviewDto } from './dto/update-restroom-review.dto';
 
 @Controller('restroom')
 export class RestroomController {
   constructor(private readonly restroomService: RestroomService) {}
+
+  // GET /restroom/filter
+  @Get('filter')
+  getFilterOptions() {}
+
+  // GET /restroom
+  @Get()
+  getRestroomList() {}
+
+  // GET /restroom/:id
+  @Get(':id')
+  getRestroomDetail(@Param('id') id: string) {}
+
+  // POST /restroom
+  @Post()
+  createRestroom() {}
+
+  // POST /restroom/:id/review
+  @Post(':id/review')
+  createRestroomReview(@Param('id') id: string) {}
 
   // PATCH /restroom/:id/review
   @Patch(':id/review')
@@ -14,4 +45,16 @@ export class RestroomController {
   ) {
     return id;
   }
+
+  // DELETE /restroom/:id/review/
+  @Delete(':id/review')
+  deleteRestroomReview(@Param('id') id: string) {}
+
+  // POST /restroom/review/:id/vote
+  @Post('review/:id/vote')
+  changeVoteStatus(@Param('id') id: string) {}
+
+  // GET /restroom/creation
+  @Get('creation')
+  getAdminReportList() {}
 }
