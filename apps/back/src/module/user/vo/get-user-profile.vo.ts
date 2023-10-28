481459c74480ccbf6a72fd0b9154548ffe0e6ffd
s22a55m77 <s22a55m77@gmail.com>
2023-10-28 09:17:59 +0800

    add vo

---
+++ apps/back/src/module/user/vo/get-user-profile.vo.ts
@@ -0,0 +1,22 @@
+type UserHistory = {
+  id: string;
+  title: string;
+  content: string;
+  rating?: number;
+  commentTo?: string; // comment ID to which it comments to
+  commentToUser?: string; // username
+  commentByUserId: string; // userId
+  commentBy?: string; // username
+  type: 'Reply' | 'Submit' | 'Review';
+};
+
+export class GetUserProfileVo {
+  id: string;
+  username: string;
+  reviews: number;
+  dateRegistered: number;
+  description: string;
+  role: 'USER' | 'ADMIN';
+  profilePicId: string;
+  history: UserHistory[];
+}
---
+++ apps/back/src/module/user/vo/update-profile-pic.vo.ts
@@ -0,0 +1,3 @@
+export class UpdateProfilePicVo {
+  id: string;
+}
