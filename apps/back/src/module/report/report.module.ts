db597f9d0f728bc8ce0b71569ae0889919ea0a83
s22a55m77 <s22a55m77@gmail.com>
2023-10-28 09:21:52 +0800

    fix module

--- apps/back/src/module/report/report.module.ts
+++ apps/back/src/module/report/report.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportEntity } from '../../model/report.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReportEntity])],
   controllers: [ReportController],
   providers: [ReportService],
 })
--- apps/back/src/module/restroom/restroom.module.ts
+++ apps/back/src/module/restroom/restroom.module.ts
@@ -4,9 +4,26 @@ import { RestroomController } from './restroom.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegionEntity } from '../../model/region.entity';
 import { TagEntity } from '../../model/tag.entity';
+import { RestroomEntity } from '../../model/restroom.entity';
+import { ImageEntity } from '../../model/image.entity';
+import { BuildingEntity } from '../../model/building.entity';
+import { ProvinceEntity } from '../../model/province.entity';
+import { FloorEntity } from '../../model/floor.entity';
+import { CommentEntity } from '../../model/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RegionEntity, TagEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      RegionEntity,
+      TagEntity,
+      RestroomEntity,
+      ImageEntity,
+      BuildingEntity,
+      ProvinceEntity,
+      FloorEntity,
+      CommentEntity,
+    ]),
+  ],
   controllers: [RestroomController],
   providers: [RestroomService],
 })
