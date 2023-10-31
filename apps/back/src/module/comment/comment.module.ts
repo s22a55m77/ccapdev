eb6369f7da979c8600605678ab9b2ee506ab5482
hyisangie <angeline_shi@dlsu.edu.ph>
2023-10-31 11:18:48 +0800

    add dependencies

--- apps/back/src/module/comment/comment.module.ts
+++ apps/back/src/module/comment/comment.module.ts
@@ -3,10 +3,18 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from '../../model/comment.entity';
+import { VoteEntity } from 'src/model/vote.entity';
+import { UserService } from '../user/user.service';
+import { UserEntity } from 'src/model/user.entity';
+import { AuthController } from '../auth/auth.controller';
+import { AuthService } from '../auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CommentEntity, VoteEntity, UserEntity]),
+  ],
   controllers: [CommentController],
-  providers: [CommentService],
+  providers: [CommentService, UserService, AuthService, JwtService],
 })
 export class CommentModule {}
--- apps/back/src/module/report/report.module.ts
+++ apps/back/src/module/report/report.module.ts
@@ -3,10 +3,40 @@ import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportEntity } from '../../model/report.entity';
+import { UserService } from '../user/user.service';
+import { RestroomService } from '../restroom/restroom.service';
+import { UserEntity } from 'src/model/user.entity';
+import { RestroomEntity } from 'src/model/restroom.entity';
+import { RegionEntity } from 'src/model/region.entity';
+import { TagEntity } from 'src/model/tag.entity';
+import { RestroomTagEntity } from 'src/model/restroom-tag.entity';
+import { ImageEntity } from 'src/model/image.entity';
+import { ProvinceEntity } from 'src/model/province.entity';
+import { CityEntity } from 'src/model/city.entity';
+import { CommentEntity } from 'src/model/comment.entity';
+import { BuildingEntity } from 'src/model/building.entity';
+import { FloorEntity } from 'src/model/floor.entity';
+import { VoteEntity } from 'src/model/vote.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ReportEntity,
+      UserEntity,
+      RestroomEntity,
+      RegionEntity,
+      TagEntity,
+      RestroomTagEntity,
+      ImageEntity,
+      ProvinceEntity,
+      CityEntity,
+      CommentEntity,
+      BuildingEntity,
+      FloorEntity,
+      VoteEntity,
+    ]),
+  ],
   controllers: [ReportController],
-  providers: [ReportService],
+  providers: [ReportService, UserService, RestroomService],
 })
 export class ReportModule {}
