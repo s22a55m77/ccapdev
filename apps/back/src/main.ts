b5efe0a79fc2ed744bc29a0922e2b8bdd3d21bd6
s22a55m77 <s22a55m77@gmail.com>
2023-11-02 15:27:06 +0800

    add env for port

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { middleware } from 'express-ctx';
 import { ExceptionsFilter } from './common/exceptions.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(middleware);
   app.enableCors();
   app.useGlobalFilters(new ExceptionsFilter());
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  await app.listen(port || 3000);
 }
 bootstrap();
