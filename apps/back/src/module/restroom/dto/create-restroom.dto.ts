b2e54dd63b68c51dada97cdc607bbda21d2c1e6f
hyisangie <angeline_shi@dlsu.edu.ph>
2023-11-20 15:12:30 +0800

    validation

--- apps/back/src/module/restroom/dto/create-restroom.dto.ts
+++ apps/back/src/module/restroom/dto/create-restroom.dto.ts
@@ -1,12 +1,28 @@
+import {
+  IsEnum,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsNumberString,
+  IsString,
+} from 'class-validator';
 import { GenderType } from 'src/model/restroom.entity';
 
 export class CreateRestroomDto {
+  @IsString()
   location: string;
+  @IsNumberString()
   region: number;
+  @IsNumberString()
   province: number;
+  @IsNumberString()
   city: number;
+  @IsString()
   building: string;
+  @IsNumberString()
   floor: number;
+  @IsIn(['male', 'female'])
   gender: GenderType;
+  @IsString()
   tags: string; // front use JSON.stringify, original type is string[]
 }
