1ced09af4ee9bd95e0c4262e7c45507360f322f1
s22a55m77 <s22a55m77@gmail.com>
2023-11-25 13:12:51 +0800

    add cors origin for deployment

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -11,7 +11,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(middleware);
   app.enableCors({
-    origin: ['http://localhost:5173', 'localhost'],
+    origin: [
+      'http://localhost:5173',
+      'localhost',
+      'https://ccapdev-front.vercel.app/',
+      'https://ccapdev.azurewebsites.net/',
+    ],
     credentials: true,
   });
   app.useGlobalFilters(new ExceptionsFilter());
